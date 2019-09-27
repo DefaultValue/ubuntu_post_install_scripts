37b1c4b7fd0dacc61c1188f84409a9f4b0c52603
Maksym Zaporozhets <maksimz@default-value.com>
2019-09-27 15:12:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,130 @@
+# Docker-based PHP development infrastructure. From clean Ubuntu to deployed Magento 2 in 5 commands. #
+
+This is a part of the local infrastructure project which aims to create easy to install and use environment for PHP development based on Ubuntu LTS.
+
+1. `Ubuntu post-installation scripts` (this repository) - install software,
+clone repositories with `Docker infrastructure` and `Dockerizer for PHP` tool. Infrastructure is launched automatically
+during setup and you do not need start it manually. Read below information to get more info about what software is installed,
+where the files are located and why we think this software is needed.
+
+2. [Docker infrastructure](https://github.com/DefaultValue/docker_infrastructure) - run [Traefik](https://traefik.io/)
+reverse-proxy container with linked MySQL 5.6, 5.7 and phpMyAdmin containers. Infrastructure is cloned and run automatically by the
+[Ubuntu post-installation scripts](https://github.com/DefaultValue/ubuntu_post_install_scripts). Check this repository
+for more information on how the infrastructure works, how to use xDebug, LiveReload etc.
+
+3. [Dockerizer for PHP](https://github.com/DefaultValue/dockerizer_for_php) - install any Magento 2 version in 1
+command. Add Docker files to your existing PHP projects in one command. This repository is cloned automatically
+by the [Ubuntu post-installation scripts](https://github.com/DefaultValue/ubuntu_post_install_scripts). Please, check
+[Dockerizer for PHP](https://github.com/DefaultValue/dockerizer_for_php) repository to get more information on available
+commands and what the tool does.
+
+
+## Environment ##
+
+Automated environment installation script for Ubuntu 18.04 LTS x64 and above. Download it and run:
+
+```bash
+sh ubuntu_18.04.sh
+```
+
+**Important!**
+
+Do not run it with `sudo` or when you switch to the root user. Never. Otherwise a lot of things may have
+insufficient permissions.
+
+
+##Web-server application stack##
+
+See [Docker infrastructure](https://github.com/DefaultValue/docker_infrastructure) and [Dockerizer for PHP](https://github.com/DefaultValue/dockerizer_for_php)
+
+
+##Ubuntu 18.04 aliases##
+
+The following aliases are added to the `/etc/bash.bashrc` file:
+- `PHP56` - switch to PHP 5.6
+- `PHP70` - switch to PHP 7.0
+- `PHP71` - switch to PHP 7.1
+- `PHP72` - switch to PHP 7.2
+- `PHP73` - switch to PHP 7.3
+- `MY56` - connect to MySQL 5.6 server in the `mysql56` docker container (on port 3356, the same as `mysql -uroot -proot -h127.0.0.1 --port=3356 --show-warnings`)
+- `MY57` - connect to MySQL 5.7 server in the `mysql57` docker container (on port 3357, the same as `mysql -uroot -proot -h127.0.0.1 --port=3357 --show-warnings`)
+
+
+##Applications for development##
+- cUrl
+- Git &amp; Git Gui
+- Node Package Manager (NPM) &amp; Grunt - needed
+- HomeBrew and mkcert
+- Magento 1 coding standards for static code analysis and Code Sniffer in PHPStorm - https://github.com/magento/marketplace-eqp
+- Magento 2 coding standards for static code analysis and Code Sniffer in PHPStorm - https://github.com/magento/magento-coding-standard
+
+##Other software##
+- `Clipit` - clipboard manager for easy copy/paste
+- `Dropbox` - files sharing (you may not use it)
+- `htop` - process manager, better than `top`
+- `Tweaks` - tuning you Ubuntu
+- `Google Chrome`
+- `KeePass2` - encrypted password storage
+- `Midnight Commander`
+- `Shutter` - a tool for making and editing screenshots
+- `Sublime Text` - text editor
+- `Vim` - console text editor
+- `VirtualBox` - virtualization tool
+
+**PHP modules that are installed:**
+- bcmath
+- bz2
+- curl
+- gd
+- imagick
+- imap
+- intl
+- mbstring
+- mcrypt
+- memcache
+- mysql
+- pear
+- recode
+- soap
+- ssh2
+- xml
+- xmlrpc
+- xdebug
+- zip
+
+`xDebug` is configured automatically and does not require additional adjustments if you use PHPStorm. `php.ini` settings are updated for development environment.
+You may also want to download the [free Microsoft IE/Edge virtual machines for VirtualBox](https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/)
+
+## Tips for developers ##
+
+1) Create bookmark for at least `/misc` folder in Nautilus (file manager) - just move the folder to the left sidebar of Nautilus to add it to bookmarks.
+
+2) Use Ubuntu `Startup Applications` to automate launching apps on system startup.
+
+3) Use `Guake` dropdown terminal as an alternative to Terminal application. Set to to, for example, F1 key. Set switching tabs to ALT+1, ALT+2 and so on because this is a default shortkut for many other apps.
+
+4) Enable workspaces in Ubuntu and learn how to use them (if not yet) and how to move windows between workspaces. You may like using static number of workspaces instead of dynamically adding/removing them. 
+
+If you prefer to loop through the current workspace apps only rather than all apps on all workspaces:
+
+```bash
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+```
+
+5) Use `Tweaks` to fine-tune your system.
+
+6) It is possible to customize terminal output to show current time and Git branch when you-re inside the repository. Use $PS1 like this in your `~/.bashrc`:
+
+```bash
+PS1='\[\033[01;35m\][\d \t] \[\033[01;33m\]\w\[\033[01;31m\]\[\033[01;34m\]$(__git_ps1)\[\033[01;31m\] > \[\033[01;32m\]'
+```
+
+Or take this one as an example and modify it for your needs (but be sure to backup the file before that).
+
+
+## Author and maintainer ##
+
+[Maksym Zaporozhets](mailto:maksimz@default-value.com)
+
+[Magento profile](https://u.magento.com/certification/directory/dev/180177/)
+
---
+++ ubuntu_18.04.sh
@@ -0,0 +1,309 @@
+#!/usr/bin/env bash
+# sudo access will be requested if the script was not run with sudo or under root user
+sudo -k
+
+# This causes the following error: ubuntu_18.04.sh: 24: [: =: unexpected operator
+# Need to fix it, but the things work fine
+if ! [ $(sudo id -u) = 0 ]; then
+    echo "\033[31;1m"
+    echo "Root password was not entered correctly!"
+    exit 1;
+fi
+
+    printf "\n>>> Creating files and folders... >>>\n"
+sudo mkdir -p /misc/apps /misc/db /misc/share/ssl # "db" dof dumps and "share" for documents shared with the virtual machines
+sudo chmod 777 -R /misc/
+sudo chown ${USER}:${USER} -R /misc/
+
+# Install cUrl
+    printf "\n>>> cUrl is going to be installed >>>\n"
+sudo apt-get install curl -y
+
+    printf "\n>>> Adding repositories and updating software list >>>\n"
+# PHP
+sudo add-apt-repository ppa:ondrej/php -y # various PHP versions
+# sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" # not sure if this is still needed
+
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# VirtualBox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+
+    printf "\n>>> Running Ubuntu upgrade >>>\n"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install net-tools -y # ifconfig since 18.04
+
+# Install Tilda
+    printf "\n>>> Guake terminal is going to be installed >>>\n"
+sudo apt-get install guake -y
+# https://bugs.launchpad.net/ubuntu/+source/guake/+bug/1760621
+# https://github.com/Guake/guake/commit/f8699b4be6c058fd58a33a1d783cd404e9076b0e#diff-e41ec8d6a3b1c2088836632a5c13ec40
+sudo sed -i "s/            self\.delete_tab(self/        self\.delete_tab(self/" /usr/lib/python3/dist-packages/guake/guake_app.py
+
+# Install Sublime Text editor
+    printf "\n>>> Sublime Text is going to be installed >>>\n"
+sudo snap install sublime-text --classic
+
+# Install Midnight Commander
+    printf "\n>>> Midnight Commander is going to be installed >>>\n"
+sudo apt-get install mc -y
+
+# Install Vim text editor
+    printf "\n>>> Vim is going to be installed >>>\n"
+sudo apt-get install vim -y
+
+# Install htop utility
+    printf "\n>>> htop is going to be installed >>>\n"
+sudo apt-get install htop -y
+
+# Install Git and Git Gui
+    printf "\n>>> Git and Git Gui are going to be installed >>>\n"
+sudo apt-get install git git-gui -y
+
+# Install Docker + Docker-compose
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+# https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+    printf "\n>>> Docker and docker-compose are going to be installed >>>\n"
+sudo apt-get install mysql-client -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# This is to execute Docker command without sudo. Will work after logout/login because permissions should be refreshed
+sudo usermod -aG docker ${USER}
+
+# docker-compose - https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+# Install MySQL client and MySQL servers 5.6 + 5.7 from Docker images
+    printf "\n>>> Traefik, MySQL 5.6, 5.7 and phpMyAdmin are going to be installed via docker-compose - https://github.com/DefaultValue/docker_infrastructure >>>\n"
+cd /misc/apps
+git clone https://github.com/DefaultValue/docker_infrastructure.git
+cd /misc/apps/docker_infrastructure/local_infrastructure
+cp traefik_rules/rules.toml.dist traefik_rules/rules.toml
+# run docker-compose this way because we need not to log out in order to refresh permissions
+sudo docker-compose up -d
+echo "
+127.0.0.1 phpmyadmin.docker.local" | sudo tee -a /etc/hosts
+
+# Install PHP common packages
+    printf "\n>>> Install common PHP packages (php-pear php-imagick php-memcached php-ssh2 php-xdebug) and composer >>>\n"
+sudo apt-get install php-pear php-imagick php-memcached php-ssh2 php-xdebug --no-install-recommends -y
+sudo apt-get install composer -y
+
+# Install PHP 5.6 and modules
+    printf "\n>>> PHP 5.6 and common modules are going to be installed >>>\n"
+sudo apt-get install php5.6 php5.6-cli php5.6-common php5.6-json php5.6-opcache php5.6-readline --no-install-recommends -y
+sudo apt-get install php5.6-bz2 php5.6-bcmath php5.6-curl php5.6-gd php5.6-imap php5.6-intl php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-recode php5.6-soap php5.6-xml php5.6-xmlrpc php5.6-zip -y
+
+# Install PHP 7.0 and modules, enable modules
+    printf "\n>>> PHP 7.0 and common modules are going to be installed >>>\n"
+sudo apt-get install php7.0 php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline --no-install-recommends -y
+sudo apt-get install php7.0-bz2 php7.0-bcmath php7.0-curl php7.0-gd php7.0-imap php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-recode php7.0-soap php7.0-xml php7.0-xmlrpc php7.0-zip -y
+
+# Install PHP 7.1 and modules, enable modules
+    printf "\n>>> PHP 7.1 and common modules are going to be installed >>>\n"
+sudo apt-get install php7.1 php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-readline --no-install-recommends -y
+sudo apt-get install php7.1-bz2 php7.1-bcmath php7.1-curl php7.1-gd php7.1-imap php7.1-intl php7.1-mbstring php7.1-mcrypt php7.1-mysql php7.1-recode php7.1-soap php7.1-xml php7.1-xmlrpc php7.1-zip -y
+
+# Install PHP 7.2 and modules, enable modules
+    printf "\n>>> PHP 7.2 and common modules are going to be installed >>>\n"
+sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-readline --no-install-recommends -y
+sudo apt-get install php7.2-bz2 php7.2-bcmath php7.2-common php7.2-curl php7.2-gd php7.2-imap php7.2-intl php7.2-mbstring php7.2-mysql php7.2-recode php7.2-soap php7.2-xml php7.2-xmlrpc php7.2-zip -y
+
+# Install PHP 7.3 and modules, enable modules
+    printf "\n>>> PHP 7.3 and common modules are going to be installed >>>\n"
+sudo apt-get install php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline --no-install-recommends -y
+sudo apt-get install php7.3-bz2 php7.3-bcmath php7.3-curl php7.3-gd php7.3-imap php7.3-intl php7.3-mbstring php7.3-mysql php7.3-recode php7.3-soap php7.3-xml php7.3-xmlrpc php7.3-zip -y
+
+# Set default PHP version to 7.2
+    printf "Enabling PHP 7.2 by default"
+sudo update-alternatives --set php /usr/bin/php7.2
+
+    printf "\n>>> Install composer package for paralell dependency downloads hirak/prestissimo globally >>>\n"
+composer global require hirak/prestissimo
+
+    printf "\n>>> Creating ini files for the development environment >>>\n"
+IniDirs=/etc/php/*/*/conf.d/
+for IniDir in ${IniDirs};
+do
+    printf "Creating ${IniDir}/999-custom-config.ini\n"
+sudo rm ${IniDir}999-custom-config.ini
+echo "error_reporting=E_ALL & ~E_DEPRECATED
+display_errors=On
+display_startup_errors=On
+ignore_repeated_errors=On
+cgi.fix_pathinfo=1
+max_execution_time=3600
+session.gc_maxlifetime=84600
+
+opcache.enable=1
+opcache.validate_timestamps=1
+opcache.revalidate_freq=1
+opcache.max_wasted_percentage=10
+opcache.memory_consumption=256
+opcache.max_accelerated_files=20000
+
+xdebug.remote_enable=1
+xdebug.remote_handler=dbgp
+xdebug.remote_mode=req
+xdebug.remote_host=127.0.0.1
+xdebug.remote_port=9000
+xdebug.max_nesting_level=256
+" | sudo tee ${IniDir}999-custom-config.ini > /dev/null
+done
+
+IniDirs=/etc/php/*/cli/conf.d/
+for IniDir in ${IniDirs};
+do
+echo "memory_limit=2G
+" | sudo tee -a ${IniDir}999-custom-config.ini >> /dev/null
+done
+
+    printf "\n>>> Enabling php modules: mbstring mcrypt xdebug >>>/n"
+sudo phpenmod mbstring mcrypt xdebug
+
+    printf "\n>>> Creating aliases and enabling color output >>>\n"
+# XDEBUG_CONFIG is important for CLI debugging
+echo "
+force_color_prompt=yes
+shopt -s autocd
+set completion-ignore-case On
+
+export XDEBUG_CONFIG=\"idekey=PHPSTORM\"
+
+alias PHP56=\"sudo update-alternatives --set php /usr/bin/php5.6 > /dev/null\"
+alias PHP70=\"sudo update-alternatives --set php /usr/bin/php7.0 > /dev/null\"
+alias PHP71=\"sudo update-alternatives --set php /usr/bin/php7.1 > /dev/null\"
+alias PHP72=\"sudo update-alternatives --set php /usr/bin/php7.2 > /dev/null\"
+alias PHP73=\"sudo update-alternatives --set php /usr/bin/php7.3 > /dev/null\"
+
+alias MY56=\"mysql -uroot -proot -h127.0.0.1 --port=3356 --show-warnings\"
+alias MY57=\"mysql -uroot -proot -h127.0.0.1 --port=3357 --show-warnings\"" | sudo tee -a /etc/bash.bashrc > /dev/null
+
+# Install a tool for PHP projects dockerization and fast Magento installation
+    printf "\n>>> Installing Dockerizer for PHP tool - https://github.com/DefaultValue/dockerizer_for_php >>>\n"
+cd /misc/apps
+git clone https://github.com/DefaultValue/dockerizer_for_php.git
+cd ./dockerizer_for_php/
+composer install
+
+# Install Node Package Manager and Grunt tasker
+# NodeJS is needed to run JSCS and ESLint for M2 in PHPStorm
+# @TODO: not sure that Grunt is still needed
+    printf "\n>>> NPM and Grunt are going to be installed >>>/n"
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install nodejs -y
+# sudo apt-get install build-essential -y
+sudo npm install -g grunt-cli
+
+    printf "\n>>> LiveReload extension is going to be clonned and built - https://github.com/lokcito/livereload-extensions >>>\n"
+cd /misc/apps/
+git clone https://github.com/lokcito/livereload-extensions.git
+cd ./livereload-extensions/
+npm install
+grunt chrome
+
+# Install Java Runtime Environment and VirtualBox
+    printf "\n>>> JDK and VirtualBox are going to be installed >>>\n"
+sudo apt install virtualbox-6.0 -y
+    printf "\n>>> Adding VirtualBox user to your group, so it can access USB devices >>>\n"
+sudo usermod -a -G vboxusers ${USER}
+
+# Install Google Chrome
+    printf "\n>>> Google Chrome is going to be installed \n>>>"
+sudo apt-get install google-chrome-stable -y
+
+# Install Homebrew and mkcert
+    printf "\n>>> Homebrew and mkcert are going to be installed - https://github.com/FiloSottile/mkcert >>>\n"
+sudo apt-get install build-essential file libnss3-tools -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)" < /dev/null
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+brew install mkcert
+mkcert -install
+
+# Install Shutter
+# @TODO: must substitute it with some better Ubuntu tool for making and editing screenshots
+    printf "\n>>> Shutter is going to be installed >>>\n"
+sudo apt-get install shutter -y
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
+sudo dpkg -i libgoocanvas3_1.0.0-1_amd64.deb; sudo apt-get -f install -y
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+sudo dpkg -i libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb; sudo apt-get -f install -y
+rm -f libgoocanvas-common_1.0.0-1_all.deb libgoocanvas3_1.0.0-1_amd64.deb libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+
+# Install Keepass2 - free encrypted password storage
+    printf "\n>>> Keepass2 is going to be installed >>>\n"
+sudo apt-get install keepass2 -y
+
+# Install Dropbox
+    printf "\n>>> Dropbox is going to be installed >>>\n"
+sudo apt-get install nautilus-dropbox -y
+sudo nautilus --quit
+
+# Install ClipIt clipboard manager
+    printf "\n>>> ClipIt clipboard manager is going to be installed >>>\n"
+sudo apt-get install clipit -y
+
+# Install Slack messanger
+    printf "\n>>> Slack messanger is going to be installed >>>\n"
+sudo snap install slack --classic
+
+# Install PHPStorm EAP (Early Access Program) that is free. Use licensed version if you have it!
+    printf "\n>>> PHPStorm EAP is going to be installed >>>\n"
+sudo snap install phpstorm --classic --edge
+    printf "\n>>> Setting filesystem parameters for PHPStorm IDE: fs.inotify.max_user_watches = 524288 >>>\n"
+echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf > /dev/null
+
+# Install Gnome Tweak Tool for tuning Ubuntu
+    printf "\n>>> Gnome Tweak Tool is going to be installed >>>\n"
+sudo apt-get install gnome-tweak-tool -y
+
+# @TODO: check if Magento repo keys are needed for this, do not run 'composer install' otherwise
+    printf "\n>>> Magento 2 coding standards - https://github.com/magento/magento-coding-standard >>>\n"
+cd /misc/apps/
+git clone https://github.com/magento/magento-coding-standard.git
+cd magento-coding-standard
+composer install
+
+    printf "\n>>> Magento 1 coding standards - https://github.com/magento/marketplace-eqp >>>\n"
+cd /misc/apps/
+git clone https://github.com/magento/marketplace-eqp.git
+cd marketplace-eqp
+composer install
+
+# System reboot
+    printf "\033[31;1m"
+read -p "/**********************
+*
+*    ATTENTION!
+*
+*    System is going to be restarted
+*
+*    More information is in the repository:
+*    https://gitlab.allbugs.info/maksymz/magento_team_scripts
+*    (open and save the URL to bookmarks)
+*
+*    PRESS ANY KEY TO CONTINUE
+*
+\**********************
+" nothing
+
+printf "\n*** Job done! Going to reboot in 5 seconds... ***\n"
+
+sleep 5
+sudo reboot
